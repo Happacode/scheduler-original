8dbc6bbe672fdb1866f83dfda9a47d7647b812d7
Harp Sandhu <happacode@protonmail.com>
2021-05-11 19:11:14 -0700

    Add button component refactor

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,19 @@
 import React from "react";
-
+import classnames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
-
-  if (props.confirm) {
-    buttonClass += " button--confirm";
-  }
-
-  if (props.danger) {
-     buttonClass += " button--danger";
-  }
+  
+  const buttonClass = classnames("button", {
+   "button--confirm": props.confirm,
+   "button--danger": props.danger
+  });
 
   return (
-   <button 
-      className={buttonClass} 
-      disabled={props.disabled} 
+   <button
+      className={buttonClass}
       onClick={props.onClick}
+      disabled={props.disabled}
    >
       {props.children}
    </button>
