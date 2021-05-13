9ebb8c8ac504739bae37459801af9fe54bdf0dd5
Harp Sandhu <happacode@protonmail.com>
2021-05-12 21:21:34 -0700

    Add DayLightItem test

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,10 +10,24 @@ export default function DayListItem(props) {
     "day-list_item--full": props.spots === 0
   })
   
+  const formatSpots = (spots) => {
+    let spotsAvailable;
+    if (spots === 0) {
+      spotsAvailable = "no spots remaining";
+    }
+    if (spots === 1) {
+      spotsAvailable = "1 spot remaining";
+    }
+    if (spots === 2) {
+      spotsAvailable = "2 spots remaining";
+    }
+    return spotsAvailable;
+  }
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
